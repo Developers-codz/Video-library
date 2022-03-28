11f568b2299b693b997801b352dbd1b2605a1c92
developers-codz <kaj123al2002@gmail.com>
2022-03-28 17:23:50 +0530

    refactor: data for aside

--- src/components/aside/Aside.jsx
+++ src/components/aside/Aside.jsx
@@ -1,6 +1,8 @@
 import style from "./aside.module.css";
 import { NavLink } from "react-router-dom";
 import { useAside } from "context/aside-context";
+import { asideData } from "data";
+
 export const Aside = () => {
   const { activeAside } = useAside();
   return (
@@ -10,18 +12,17 @@ export const Aside = () => {
         style={activeAside ? { left: "0%" } : { left: "-50%" }}
       >
         <ul className={style.asideUl}>
-          <NavLink className="decor-none light-text" exact="true" to="/">
-            <li className={style.asideLink}>Home</li>
-          </NavLink>
-          <NavLink className="decor-none light-text" to="/playlist">
-            <li className={style.asideLink}>Playlist</li>
-          </NavLink>
-          <NavLink className="decor-none light-text" to="/history">
-            <li className={style.asideLink}>History</li>
-          </NavLink>
-          <NavLink className="decor-none light-text" to="/liked">
-            <li className={style.asideLink}>Liked</li>
-          </NavLink>
+          {asideData.map(({ name, linkTo }) => {
+            return (
+              <NavLink
+                className="decor-none light-text"
+                exact="true"
+                to={linkTo}
+              >
+                <li className={style.asideLink}>{name}</li>
+              </NavLink>
+            );
+          })}
         </ul>
       </div>
     </>
---
+++ src/data/index.js
@@ -0,0 +1,1 @@
+export { asideData } from "./aside-data";
