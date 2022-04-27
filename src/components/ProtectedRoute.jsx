f3576b7e4fc6169260de052ed088e99c402599d8
developers-codz <kaj123al2002@gmail.com>
2022-04-27 21:50:07 +0530

    feat:persisting auth

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,10 +1,12 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet,useLocation } from "react-router-dom";
 import { useAuth } from "context/auth-context";
 
 const ProtectedRoute = () => {
   const {
     authState: { authToken },
   } = useAuth();
-  return authToken === "" ? <Navigate to="/login" /> : <Outlet />;
+  const location = useLocation();
+  console.log(location)
+  return authToken === "" ? <Navigate to="/login" state={{from: location}} /> : <Outlet />;
 };
 export { ProtectedRoute };
--- src/components/PublicRoute.js
+++ src/components/PublicRoute.js
@@ -1,11 +1,13 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet,useLocation} from "react-router-dom";
 import { useAuth } from "context/auth-context";
 
 const PublicRoute = () => {
   const {
     authState: { authToken },
   } = useAuth();
-  return authToken === "" ? <Outlet /> : <Navigate to="/" />;
+  const location = useLocation()
+  console.log(location)
+  return authToken === "" ? <Outlet /> : <Navigate to={location.state.from.pathname} />;
 };
 
 export { PublicRoute };
