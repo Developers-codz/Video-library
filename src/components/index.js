c00b7b7da07c5dc06af8ebfc085f61999c700e33
developers-codz <kaj123al2002@gmail.com>
2022-04-27 21:37:45 +0530

    feat: bottom nav done

--- src/components/index.js
+++ src/components/index.js
@@ -8,3 +8,4 @@ export { SingleCard } from "./cards/SingleCard";
 export { Playlistcard } from "./playlistcard/Playlistcard";
 export { Modal } from "./modal/Modal";
 export { Loader } from "./loader/Loader";
+export {BottomNavigation} from "./bottomNavigation/BottomNavigation"
